535e173f1feafa768ec8006eba0a3afc70c991df
Patel, Deepankumar Maheshbhai <deepankumar.patel1@ge.com>
2020-04-07 11:05:10 -0700

    1. Adding Node_modules to gitignore 2. adding bubblesort and selectsort algorithms

--- sorting/index.js
+++ sorting/index.js
@@ -2,11 +2,34 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for(let i = 0; i< arr.length; i++){
+        let tempVal;
+        for(let j = 0; j < arr.length - i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                tempVal = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tempVal;
+            }
+        }
+    }
+    return arr;
 }
 
 function selectionSort(arr) {
-
+    for(let i = 0; i < arr.length; i++) {
+        let minValIndex = i;
+        for(let j = i+1; j < arr.length; j++) {
+            if(arr[j] < arr[minValIndex]) {
+                minValIndex = j;
+            }
+        }
+            if(minValIndex !== i){
+                let tempVal = arr[minValIndex];
+                arr[minValIndex] = arr[i];
+                arr[i] = tempVal;
+            }
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
