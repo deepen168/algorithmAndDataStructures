4dd31204c41c91b1ea223bb66abaaf9d81e66a9b
Deepan Patel <deepanpatel@Deepans-MBP.hsd1.ca.comcast.net>
2022-08-19 16:47:57 -0700

    adding google 1 unique email address problem

---
+++ leetcode/apple/romanToInt/romanToInt.js
@@ -0,0 +1,4 @@
+function romanToInt() { 
+
+}; 
+export { romanToInt };
---
+++ leetcode/apple/romanToInt/romanToInt.test.js
@@ -0,0 +1,7 @@
+import { romanToInt } from "./romanToInt";
+
+const input = [];
+const resultVal = [];
+test('check if solution works', () => {
+    expect(romanToInt(input)).toEqual(resultVal);
+});
