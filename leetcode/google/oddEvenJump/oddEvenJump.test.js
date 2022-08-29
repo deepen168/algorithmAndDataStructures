ec5e65f6fc4ed71f74ccb081a6516e6a01d558b7
Deell's home mac <deellshome@Deellss-Air.hsd1.ca.comcast.net>
2022-08-29 06:24:27 -0700

    adding all google

---
+++ leetcode/google/oddEvenJump/oddEvenJump.test.js
@@ -0,0 +1,7 @@
+const oddEvenJump = require('./oddEvenJump.js');
+
+            const input = [];
+            const resultVal = [];
+            test('check if solution works', () => {
+            expect(oddEvenJump(input)).toEqual(resultVal);
+            });
