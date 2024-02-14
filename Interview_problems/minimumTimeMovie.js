b1cdf72c477dc93113c29d444fd83282dd790053
Deepan Patel <deepen168@gmail.com>
2024-02-14 10:06:29 -0800

    2024

--- Interview_problems/minimumTimeMovie.js
+++ Interview_problems/minimumTimeMovie.js
@@ -1,4 +1,32 @@
 const movies = [1.9, 1.04, 1.25, 2.5, 1.05];
+const val1 = [1.01, 2.4, 1.01, 1.01, 1.4]
+const val3 = [1.01, 2.4, 1.4, 1.6, 2.6, 1.7]
+const val2 = [1.01, 2.4, 1.5, 1.6, 2.6, 1.7]
+
+const finDays = (movies) => {
+	const sortedMovieTimes = movies.sort((a,b) => a-b);
+	let days = 0;
+  let first = 0;
+  let last = sortedMovieTimes.length - 1;
+  
+  while(first <= last) { 
+      days++;
+  	if(sortedMovieTimes[first] + sortedMovieTimes[last] <= 3 ) { 
+        first++;
+        last--;
+    } else {
+        last--;
+    }
+  }
+    return days
+}
+
+console.log(finDays(movies))
+console.log(finDays(val1))
+console.log(finDays(val2))
+console.log(finDays(val3))
+
+//****************************************************************************
 
 function findDaysToComplete(arr) {
   let days = 0;
